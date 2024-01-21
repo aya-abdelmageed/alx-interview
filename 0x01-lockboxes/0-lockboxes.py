92f11f19b0d1ab0e1bd8c271ddba42194f49d377
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2024-01-22 01:12:16 +0200

    add task

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    # method that determines if all the boxes can be opened.
+    
+    n = len(boxes)
+    opened = [0]
+
+    for key, box in enumerate(boxes):
+        
+        for k in box:
+            if k not in opened and k != key:
+                opened.append(k)
+
+    if len(opened) == n:
+        return True
+    else:
+        return False
+
+
+        
---
+++ 0x01-lockboxes/README.md
@@ -0,0 +1,1 @@
+# This is a 0x01-lockboxes README.md file
