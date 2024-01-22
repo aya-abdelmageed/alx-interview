86cedcc353fed57a7a02a242e1267a794917a02c
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2024-01-22 02:09:16 +0200

    fix solution

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,17 +5,11 @@
 def minOperations(n):
     """method for the problem"""
 
-    next = 'H'
-    body = 'H'
-    op = 0
-    while (len(body) < int(n)):
-        if n % len(body) == 0:
-            op += 2
-            next = body
-            body += body
-        else:
-            op += 1
-            body += next
-    if len(body) != n:
-        return 0
-    return op
+    t = 0
+    m = 2
+    while n > 1:
+        while not n % m:
+            t += m
+            n /= m
+        m += 1
+    return t
