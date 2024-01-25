f5c6c9f1900bba6d00d03fc74bce6c6794d75658
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2024-01-25 23:39:15 +0200

    fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,14 +5,14 @@ Log parsing
 
 import sys
 
-if __name__ == '__main__':
+if name == 'main':
 
     filesize, count = 0, 0
     codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
     stats = {k: 0 for k in codes}
 
-    def print_stats(stats: dict, file_size: int) -> None:
-        print("File size: {:d}".format(filesize))
+    def print_stats(stats, file_size):
+        print("File size: {:d}".format(file_size))
         for k, v in sorted(stats.items()):
             if v:
                 print("{}: {}".format(k, v))
@@ -26,14 +26,14 @@ if __name__ == '__main__':
                 if status_code in stats:
                     stats[status_code] += 1
             except BaseException:
-                pass
+                continue
             try:
                 filesize += int(data[-1])
             except BaseException:
-                pass
+                continue
             if count % 10 == 0:
                 print_stats(stats, filesize)
         print_stats(stats, filesize)
     except KeyboardInterrupt:
         print_stats(stats, filesize)
-        raise
+        sys.exit(0)
